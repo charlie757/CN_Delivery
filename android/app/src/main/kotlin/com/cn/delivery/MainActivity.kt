d790c4079d95801a70758cfb0b2bb599a6630616
Ravi <rraviggupta15@gmail.com>
2024-07-12 20:27:42 +0530

    changes

--- android/app/src/main/kotlin/com/cn/delivery/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.cn.delivery
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ android/app/src/main/kotlin/com/consumersnetwork/delivery/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.consumersnetwork.delivery
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
